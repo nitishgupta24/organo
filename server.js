b09e62c17d992c8e7c41e5216a186fc75e9186cf
GAURAV SHARMA <gauravsharma902753@gmail.com>
2025-02-19 00:13:53 +0530

    Update  cors middleware

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require("express");
+const app = express();
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const cors = require('cors');
+
 
 dotenv.config();
 connectDB();
 
-const app = express();
 app.use(express.json());
+app.use(cors({
+  origin:"http://localhost:3000",
+  methods:["GET","POST","PUT","DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials:true
+}))
+
 
 app.get("/", (req, res) => {
   res.send("working...");
